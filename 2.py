9093dae052ab70c52701461ac84d232ca103b495
db <diogo.bernardo.dev@gmail.com>
2023-04-14 14:09:31 +0100

    sync locals

--- 2.py
+++ 2.py
@@ -1,3 +1,4 @@
+import re
 import json
 from typing import Dict
 import ply.lex as plex
@@ -23,6 +24,7 @@ class VendingMachine:
         "ITEM",
         "COIN",
         "OTHER",
+        "RESET",
     )
 
     @plex.TOKEN(r"QUANTIA")
@@ -30,6 +32,12 @@ class VendingMachine:
         """ """
 
         t.lexer.begin("inserting")
+
+    @plex.TOKEN(r"PRODUTO")
+    def t_PRODUTO(self, t):
+        """ """
+
+        t.lexer.begin("sell")
 
     @plex.TOKEN(r"REFILL")
     def t_REFILL(self, t):
@@ -55,12 +63,6 @@ class VendingMachine:
         self.client_balance += value
         print(f"valor inserido: €{value:.2f} (saldo: €{self.client_balance:.2f})")
 
-    @plex.TOKEN(r"PRODUTO")
-    def t_inserting_PRODUTO(self, t):
-        """ """
-
-        t.lexer.begin("sell")
-
     @plex.TOKEN(r"=\w+")
     def t_sell_ITEM(self, t):
         """ """
@@ -75,30 +77,42 @@ class VendingMachine:
         self.sell_product(product)
         t.lexer.begin("INITIAL")
 
-    @plex.TOKEN(r"=\w+ \d+")
+    @plex.TOKEN(r"=(\w+) (\d+)")
     def t_refill_ITEM(self, t):
-        """ """
+        """
+        Extract product name and quantity from the given token value using a regex pattern.
 
-        t.value = (t.value[1:].rstrip(".")).split(" ")
-        t.lexer.begin("INITIAL")
+        Args:
+            t: The token object with a value containing a product name and quantity.
+        """
 
-        product = t.value[0:]
-        if product not in self.products:
-            print("Invalid product item!")
-            return
+        print("hi")
+        pattern = re.compile(r"=(\w+) (\d+)")
+        match = pattern.match(t.value)
 
-        self.sell_product(product)
+        if match:
+            product_name = match.group(1)
+            quantity = int(match.group(2))
+
+            self.refill_product(product_name, quantity)
 
     @plex.TOKEN(r"CANCELAR")
     def t_ANY_CANCELAR(self, t):
         """ """
 
-        t.lexer.begin("INITIAL")
         print(f"valor devolvido: €{self.client_balance:.2f}")
 
         change = self.calculate_change(self.client_balance)
         self.update_coins(change)
         self.client_balance = 0
+
+        t.lexer.begin("INITIAL")
+
+    @plex.TOKEN(r"\.")
+    def t_ANY_RESET(self, t):
+        """ """
+
+        t.lexer.begin("INITIAL")
 
     @staticmethod
     @plex.TOKEN(r"\n+")
@@ -135,11 +149,7 @@ class VendingMachine:
         atexit.register(self._save_state)
 
     def _save_state(self):
-        """
-        Save the vending machine state to a json file.
-        The listed items, the prices and quantities,
-        available coins, and the amount for every type.
-        """
+        """ """
 
         with open("products.json", "w") as file:
             json.dump(self.products, file, indent=4)
@@ -241,6 +251,23 @@ class VendingMachine:
             print(coin)
             self.coins[coin] -= 1
 
+    def refill_product(self, product_name, quantity):
+        """
+        Update the stock of a product in the products dictionary by adding the specified quantity.
+
+        Args:
+            product_name (str): The name of the product to be updated.
+            quantity (int): The quantity to be added to the stock.
+        """
+
+        if product_name in self.products:
+            self.products[product_name]["stock"] += quantity
+            print(
+                f"Refilled {product_name} with {quantity} units. New stock: {self.products[product_name]['stock']}"
+            )
+        else:
+            print(f"Product not found: {product_name}")
+
     def process(self, data):
         """
         Process the input data to simulate the vending machine.
@@ -255,8 +282,12 @@ class VendingMachine:
 
         self.lexer.input(data)
 
-        for _ in self.lexer:
-            continue
+        for f in self.lexer:
+            print(f)
+            print("hi")
+            # continue
+
+        print("hi")
 
 
 def main():
@@ -266,7 +297,8 @@ def main():
     QUANTIA c20, c70.
     PRODUTO=Cola.
     QUANTIA c20, c10, c5, c50, c10, c5.
-    CANCELAR
+    CANCELAR.
+    REFILL Cola 2.
     """
 
     vending_machine = VendingMachine()
